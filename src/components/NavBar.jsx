1eb73732e29a833c80897510c4a8b88343e6ae7b
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-11-01 21:43:24 +0300

    set up file structure and installed dependencies

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+
+
+const NavBar = () => {
+    return (
+        <div className="bg-red-500 h-50%">
+            Hello
+
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import NavBar from './components/NavBar.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NavBar />
+  </React.StrictMode>,
+)
