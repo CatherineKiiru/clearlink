084415e5a8fb5ebcaec393e2324adbc67f64e699
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-11-03 23:21:35 +0300

    created hero component file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import NavBar from './components/NavBar.jsx'
+import Hero from './components/Hero.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
+    <Hero/>
   </React.StrictMode>,
 )
