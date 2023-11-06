e9b60b1361feb6ce76f0839d049e615430f2fed8
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-11-06 16:30:44 +0300

    installed typescript and converted files to typescript

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 function Companies() {
   return (
     <div className="py-20">
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Button } from "flowbite-react";
+import React from "react";
 
 function Hero() {
   return (
