6d35260ff2a0a81fac435a805501ed65ecb0d843
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-11-14 10:06:00 +0300

    created accordion for support section

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,45 +1,238 @@
 import React from 'react';
+import { Accordion } from 'flowbite-react';
 
-function Testimonals() {
+function Support() {
   return (
-    <div className="bg-[#F9FAFB] w-full">
-      <div className="md:py-24 md:gap-12 md:flex md:grid-col-2 max-w-[1440px] px-5 mx-auto justify-center">
-        <div className="md:max-w-[40rem] mt-10 md:mt-0 space-y-4 md:space-y-12">
-          <img
-            className="pt-10 md:pt-0 items-center md:items justify-center mx-auto"
-            src="/src/images/shopify.svg"
-            alt="shopify-logo"
-          />
-          <img className='items-center justify-center mx-auto' src="/src/images/stars.svg" alt="rating stars" />
-          <h1 className="text-center justify-center mx-auto text-text-blue font-medium md:text-[44px]">
-            ClearLink has upgraded our remote meetings. High-quality video,
-            screen sharing, and top-notch security make it essential for our
-            team.
-          </h1>
+    <div className="max-w-[1440px] px-5 mx-auto md:flex md:gap-24">
+      {/* grid 1 */}
+      <div className="w-1/3">
+        <h3>Support</h3>
+        <h1>FAQs</h1>
+        <p>
+          Everything you need to know about the product and billing. Can’t find
+          the answer you’re looking for? Please chat to our friendly team.
+        </p>
+      </div>
 
-          <div className="md:flex md:justify-between">
-            <div className="flex gap-4 items-center justify-center mx-auto">
-              <div className="">
-                <img src="/src/images/avatar.svg" alt="" />
-              </div>
-              <div>
-                <h1 className='text-[#101828] font-semibold text-xl'>Sarah Thompson</h1>
-                <p>Project Manager, Shopify</p>
-              </div>
-            </div>
-
-            <div className="items-center justify-center mx-auto flex scale-75 gap-4">
-              <img src="/src/images/carousel-arrow-left.svg" alt="" />
-              <img src="/src/images/carousel-arrow-right.svg" alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="md:scale-110 md:ml-10 mt-10 md:mt-8">
-          <img src="/src/images/contents.svg" alt="testimonials images" />
-        </div>
+      {/* grid 2 */}
+      <div className="">
+        <Accordion>
+          <Accordion.Panel className=''>
+            <Accordion.Title>
+              How many participants can join a ClearLink video conference?
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                ClearLink offers flexible meeting options. Depending on your
+                subscription plan, you can host meetings with varying numbers of
+                participants. Our plans are designed to accommodate small team
+                collaborations and large-scale webinars, ensuring you have the
+                right fit for your needs.
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                Check out this guide to learn how to&nbsp;
+                <a
+                  href="https://flowbite.com/docs/getting-started/introduction/"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  get started&nbsp;
+                </a>
+                and start developing websites even faster with components on top
+                of Tailwind CSS.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              Can I use ClearLink on multiple devices?
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Flowbite is first conceptualized and designed using the Figma
+                software so everything you see in the library has a design
+                equivalent in our Figma file.
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                Check out the
+                <a
+                  href="https://flowbite.com/figma/"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Figma design system
+                </a>
+                based on the utility classes from Tailwind CSS and components
+                from Flowbite.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              Is ClearLink compatible with other video conferencing platforms?
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                The main difference is that the core components from Flowbite
+                are open source under the MIT license, whereas Tailwind UI is a
+                paid product. Another difference is that Flowbite relies on
+                smaller and standalone components, whereas Tailwind UI offers
+                sections of pages.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                However, we actually recommend using both Flowbite, Flowbite
+                Pro, and even Tailwind UI as there is no technical reason
+                stopping you from using the best of two worlds.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Learn more about these technologies:
+              </p>
+              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+                <li>
+                  <a
+                    href="https://flowbite.com/pro/"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Flowbite Pro
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindui.com/"
+                    rel="nofollow"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Tailwind UI
+                  </a>
+                </li>
+              </ul>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              How does ClearLink ensure the security of my video conferences?
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                The main difference is that the core components from Flowbite
+                are open source under the MIT license, whereas Tailwind UI is a
+                paid product. Another difference is that Flowbite relies on
+                smaller and standalone components, whereas Tailwind UI offers
+                sections of pages.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                However, we actually recommend using both Flowbite, Flowbite
+                Pro, and even Tailwind UI as there is no technical reason
+                stopping you from using the best of two worlds.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Learn more about these technologies:
+              </p>
+              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+                <li>
+                  <a
+                    href="https://flowbite.com/pro/"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Flowbite Pro
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindui.com/"
+                    rel="nofollow"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Tailwind UI
+                  </a>
+                </li>
+              </ul>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              Do I need to download any software to use ClearLink?{' '}
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                The main difference is that the core components from Flowbite
+                are open source under the MIT license, whereas Tailwind UI is a
+                paid product. Another difference is that Flowbite relies on
+                smaller and standalone components, whereas Tailwind UI offers
+                sections of pages.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                However, we actually recommend using both Flowbite, Flowbite
+                Pro, and even Tailwind UI as there is no technical reason
+                stopping you from using the best of two worlds.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Learn more about these technologies:
+              </p>
+              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+                <li>
+                  <a
+                    href="https://flowbite.com/pro/"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Flowbite Pro
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindui.com/"
+                    rel="nofollow"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Tailwind UI
+                  </a>
+                </li>
+              </ul>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              Do I need to download any software to use ClearLink?{' '}
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                The main difference is that the core components from Flowbite
+                are open source under the MIT license, whereas Tailwind UI is a
+                paid product. Another difference is that Flowbite relies on
+                smaller and standalone components, whereas Tailwind UI offers
+                sections of pages.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                However, we actually recommend using both Flowbite, Flowbite
+                Pro, and even Tailwind UI as there is no technical reason
+                stopping you from using the best of two worlds.
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Learn more about these technologies:
+              </p>
+              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+                <li>
+                  <a
+                    href="https://flowbite.com/pro/"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Flowbite Pro
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://tailwindui.com/"
+                    rel="nofollow"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    Tailwind UI
+                  </a>
+                </li>
+              </ul>
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
       </div>
     </div>
   );
 }
 
-export default Testimonals;
+export default Support;
