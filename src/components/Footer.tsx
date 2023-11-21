4c0f864a9f3f7eddfcc611662e5ffbc05cd74998
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-11-21 11:41:19 +0300

    added scroll to top functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,21 @@
 import { Footer } from 'flowbite-react';
 import React from 'react';
 import {
-  BsDribbble,
   BsFacebook,
   BsGithub,
   BsInstagram,
   BsTwitter,
+  BsLinkedin,
+  BsArrowUpCircleFill,
 } from 'react-icons/bs';
 
 function FooterSection() {
   return (
-    <Footer className="shadow-none border-none ">
+    <Footer className="shadow-none border-none">
       <div className="w-full">
-        <div className="md:mb-12 px-5 md:px-0 container mx-auto grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          
-        <Footer.Divider className='md:hidden'/>
-        <div className="w-full md:w-1/2 lg:w-1/2 space-y-6">
+        <div className="mb-12 px-5 md:px-0 container mx-auto grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <Footer.Divider className="md:hidden" />
+          <div className="w-full md:w-1/2 lg:w-1/2 space-y-6">
             <Footer.Brand
               href=""
               src="/src/images/clearlink-logo.png"
@@ -32,7 +32,7 @@ function FooterSection() {
           <div className="grid grid-cols-5 mt-8 md:mt-0 lg:grid-cols-5 sm:gap-6 w-full">
             <div>
               <Footer.Title title="Product" />
-              <Footer.LinkGroup col className='text-xs md:text-base'>
+              <Footer.LinkGroup col className="text-xs md:text-base">
                 <Footer.Link href="#">Overview</Footer.Link>
                 <Footer.Link href="#">Features</Footer.Link>
                 <Footer.Link href="#">Solutions</Footer.Link>
@@ -42,7 +42,7 @@ function FooterSection() {
             </div>
             <div>
               <Footer.Title title="Company" />
-              <Footer.LinkGroup col className='text-xs md:text-base'>
+              <Footer.LinkGroup col className="text-xs md:text-base">
                 <Footer.Link href="#">About Us</Footer.Link>
                 <Footer.Link href="#">Careers</Footer.Link>
                 <Footer.Link href="#">Press</Footer.Link>
@@ -52,7 +52,7 @@ function FooterSection() {
             </div>
             <div>
               <Footer.Title title="Resources" />
-              <Footer.LinkGroup col className='text-xs md:text-base'>
+              <Footer.LinkGroup col className="text-xs md:text-base">
                 <Footer.Link href="#">Blog</Footer.Link>
                 <Footer.Link href="#">Events</Footer.Link>
                 <Footer.Link href="#">Help Center</Footer.Link>
@@ -63,7 +63,7 @@ function FooterSection() {
 
             <div>
               <Footer.Title title="Legal" />
-              <Footer.LinkGroup col className='text-xs md:text-base'>
+              <Footer.LinkGroup col className="text-xs md:text-base">
                 <Footer.Link href="#">Terms</Footer.Link>
                 <Footer.Link href="#">Privacy</Footer.Link>
                 <Footer.Link href="#">Cookies</Footer.Link>
@@ -73,7 +73,7 @@ function FooterSection() {
             </div>
             <div className=" lg:mt-0 ">
               <Footer.Title title="Get the app" className="text-[#004EEB] " />
-              
+
               <Footer.LinkGroup className="lg:flex lg:space-y-8 scale-150 lg:scale-100">
                 <Footer.Brand
                   href=""
@@ -89,16 +89,25 @@ function FooterSection() {
                   name="Playstore"
                 />
               </Footer.LinkGroup>
-             
-
+              <div className="mt-12 scale-150 lg:pl-20 ">
+                <Footer.Icon
+                  onClick={() => {
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                  }}
+                  href=""
+                  icon={BsArrowUpCircleFill}
+                  
+                />
+              </div>
             </div>
           </div>
         </div>
         <div className="bg-[#F9FAFB] py-6 md:py-12">
           <div className="container mx-auto w-full text-center md:flex-left sm:flex sm:items-center sm:justify-between ">
             <Footer.Copyright
-              href="#"
-              by="Clearlink™, All Rights Reserved"
+              href="https://twitter.com/CatherineKiiru"
+              target=""
+              by="Clearlink™, All Rights Reserved. Website Developed by Catherine Kiiru"
               year={2023}
             />
             <div className="mt-4 justify-center md:justify-normal flex space-x-6 sm:mt-0 sm:justify-center">
@@ -106,7 +115,7 @@ function FooterSection() {
               <Footer.Icon href="#" icon={BsInstagram} />
               <Footer.Icon href="#" icon={BsTwitter} />
               <Footer.Icon href="#" icon={BsGithub} />
-              <Footer.Icon href="#" icon={BsDribbble} />
+              <Footer.Icon href="#" icon={BsLinkedin} />
             </div>
           </div>
         </div>
--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -29,17 +29,7 @@ function Support() {
                 collaborations and large-scale webinars, ensuring you have the
                 right fit for your needs.
               </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to&nbsp;
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  className="text-cyan-600 hover:underline dark:text-cyan-500"
-                >
-                  get started&nbsp;
-                </a>
-                and start developing websites even faster with components on top
-                of Tailwind CSS.
-              </p>
+              
             </Accordion.Content>
           </Accordion.Panel>
           <Accordion.Panel>
